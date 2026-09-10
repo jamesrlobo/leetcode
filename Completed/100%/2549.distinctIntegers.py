4933e517d9efa7915406104411a6956fd2edd2e5
jamesrlobo <jamesrlobo@gmail.com>
2026-09-10 19:42:38 +0530

    Solved 2549. Count Distinct Numbers on Board

---
+++ Completed/100%/2549.distinctIntegers.py
@@ -0,0 +1,9 @@
+# 2549. Count Distinct Numbers on Board
+# https://leetcode.com/problems/count-distinct-numbers-on-board/description/
+# Beats: 100.00%
+def distinctIntegers(n):
+    return max(1,n-1)
+
+
+n = 5
+print(distinctIntegers(n))
